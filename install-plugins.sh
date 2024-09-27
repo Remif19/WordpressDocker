6dec5faed8fd9725807d6e7569f58463a762c654
Remif19 <123941695+Remif19@users.noreply.github.com>
2024-09-27 09:58:21 +0200

    Add Default Plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
-    echo "Waiting for database to be ready..."
-    sleep 5
+until wp db check --path="/var/www/html" --allow-root; do
+  echo "Waiting for database to be ready..."
+  sleep 5
 done
 
-wp plugin install jetpack --activate
-wp plugin install akismet --activate
+wp plugin install jetpack --activate --path="/var/www/html" --allow-root
+wp plugin install woocommerce --activate --path="/var/www/html" --allow-root
+wp plugin install wordfence --activate --path="/var/www/html" --allow-root
